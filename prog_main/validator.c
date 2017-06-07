78294e33e7cfb58a1961a37254de91b906466c37
PaulContremoulin <paul.contremoulin@etu.umontpellier.fr>
2017-06-08 01:56:46 +0200

    Version 2.0.0 en prod

--- prog_main/validator.c
+++ prog_main/validator.c
@@ -10,6 +10,7 @@
 #include <termios.h>
 #include <errno.h>
 #include <fcntl.h> 
+#include <time.h> 
 
 #define PORT 1123
 
@@ -80,16 +81,117 @@ char* joueur_request(char *hostname)
 }
 
 
-//Envoi des données au monitoring
-int sendMonitoring(char* rfid, char* idball) {
-    
-    int sockD, verif;
+//Fonction d'erreurs
+void error(const char *msg) { perror(msg); exit(0); }
+
+//Envoi de données a beebotte en POST
+int sendToBeBotte(char *canal, char *clefCanal, char *ressource, char *data[]) {
+  // data est un tableau de chaines (char[]), c-a-d un tableau de char a deux dimensions
+  // printf("data[0] is %s\n",data[0]);
+  //printf("data[3] is %s\n",data[3]);
+
+  int i;
+  char *host = "api.beebotte.com";
+    /* !! TODO remplacer 'testVB' par le canal dans lequel publier (ex: partie12)
+        (ici msg est la "ressource" que ce canal attend */
+  char path[100] = "/v1/data/write/";
+  strcat(path,canal);strcat(path,"/"); strcat(path,ressource);
+  struct hostent *server;
+  struct sockaddr_in serv_addr;
+  int sockfd, bytes, sent, received, total, message_size;
+  char *message, response[4096];
+
+    // Necessaire pour envoyer des donnees sur beebottle.com (noter le token du canal a la fin) :
+  char headers[300] ="Host: api.beebotte.com\r\nContent-Type: application/json\r\nX-Auth-Token: ";
+  strcat(headers,clefCanal);strcat(headers,"\r\n"); 
+
+  char donnees[4096] = "{\"data\":\""; // "data" est impose par beebotte.com
+  for (i=0;i<3;i++) {
+    strcat(donnees,data[i]);strcat(donnees,",");
+  }
+  strcat(donnees,data[3]);strcat(donnees,"\"}");
+
+  /* How big is the whole HTTP message? (POST) */
+  message_size=0;
+  message_size+=strlen("%s %s HTTP/1.0\r\n")+strlen("POST")+strlen(path)+strlen(headers);
+  message_size+=strlen("Content-Length: %d\r\n")+10+strlen("\r\n")+strlen(donnees); 
+  /* allocate space for the message */
+  message=malloc(message_size);
+
+  /* Construit le message POST */
+  sprintf(message,"POST %s HTTP/1.0\r\n",path); 
+  sprintf(message+strlen(message), "%s",headers);
+  sprintf(message+strlen(message),"Content-Length: %zu\r\n",strlen(donnees));
+  strcat(message,"\r\n");              /* blank line     */
+  strcat(message,donnees);             /* body           */
+
+  /* What are we going to send? */
+  printf("Request:\n%s\n-------------\n",message);
+
+  /* create the socket */
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) error("ERROR opening socket");
+
+  /* lookup the ip address */
+  server = gethostbyname(host);
+  if (server == NULL) error("ERROR, no such host");
+
+  /* fill in the structure */
+  memset(&serv_addr,0,sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(80); // port 80
+  memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+  /* connect the socket */
+  if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) error("ERROR connecting");
+
+  /* send the request */
+  total = strlen(message);
+  sent = 0;
+  do {
+    bytes = write(sockfd,message+sent,total-sent);
+    if (bytes < 0) error("ERROR writing message to socket");
+    if (bytes == 0) break;
+    sent+=bytes;
+  } while (sent < total);
+
+  /* receive the response */
+  memset(response,0,sizeof(response));
+  total = sizeof(response)-1;
+  received = 0;
+  do {
+    bytes = read(sockfd,response+received,total-received);
+    if (bytes < 0) error("ERROR reading response from socket");
+    if (bytes == 0) break;
+    received+=bytes;
+  } while (received < total);
+
+  if (received == total) error("ERROR storing complete response from socket");
+  
+  /* close the socket */
+  close(sockfd); 
+
+  /* process response */
+  printf("Response:\n%s\n",response);
+
+  free(message);
+  return 0;
+}
+
+//Fonction pour récupérer les données sur bebotte
+void recvToBebotte(char *channel, char *ressource, char *data) {
+
+    int sockD, verif, total, received, bytes;
     struct sockaddr_in sin;
     struct hostent *hostinfo;
+
+    char *hostname = "api.beebotte.com";
+    char response[4096];
+    char *pos;
     
     /* Configuration de la connexion */
     sin.sin_port = htons(80);
-    hostinfo = gethostbyname("www.dweet.io");
+    hostinfo = gethostbyname(hostname);
     
     sin.sin_addr = *(struct in_addr *) hostinfo->h_addr;
     sin.sin_family = AF_INET;
@@ -104,22 +206,119 @@ int sendMonitoring(char* rfid, char* idball) {
     
     /* Creation de la requête */
     char request[256];
-    sprintf   (request, "GET /dweet/for/robot17?rfid=%s&idball=%s HTTP/1.1\r\nHost:%s\r\n\r\n", rfid, idball, "www.dweet.io");
+    sprintf(request, "GET /v1/public/data/read/vberry/%s/%s HTTP/1.1\r\nHost: %s\r\n\r\n", channel, ressource, hostname);
     printf("REQUETE : \n%s\n",request);
-    printf("Valeur de rfid : %s\n",rfid);
-    printf("Valeur de idball : %s\n\n",idball);
+
     /* Envoie du message au serveur */
     verif = send(sockD, request, strlen(request),0);
-    close(sockD);
-    return 1;
+
+    /* receive the response */
+    memset(response,0,sizeof(response));
+    total = sizeof(response)-1;
+    received = 0;
+    //do {
+	//printf("receved = %d\n", received);
+	//printf("Message = %s\n", response);
+    	bytes = recv(sockD,response+received,total-received,MSG_PEEK);
+        if (bytes < 0) error("ERROR reading response from socket");
+	//if (bytes == 0) break;
+	received+=bytes;
+    //} while (received < total);
+
+    if (received == total) error("ERROR storing complete response from socket");
+
+    /* close the socket */
+    close(sockD); 
+
+    /*Get body response */
+    pos=strchr(response, '[');
+    memcpy(data, pos, sizeof(response));
+
+    /* process response */
+    //printf("Body reponse:\n%s\n\n",data);
+}
+
+//Envoi la validation du but
+int sendValidBut(char* joueur) {
+    char *infoAPublier[4];
+    char *channel = "VBpartieTEST";
+    char *ressource = "msg";
+    // VB_TC : 1496436248369_f6FG4rK7bYcY9R1u
+    // VBpartieTEST : 1496856372525_RTFvkGaUTGiDzni8
+    // RJpartieTEST : 1496856856204_4VjiqoDtPoWVTeHD
+    char *channelKey = "1496856372525_RTFvkGaUTGiDzni8";
+
+    char datenow[20];
+    time_t now = time(NULL);
+    strftime(datenow, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+  
+    char data[256] = "JOUEUR=";
+    strcat(data,joueur);strcat(data,";");strcat(data,"TIME=");strcat(data,datenow);
+    printf("\nValeur de DATA   : %s\n", data);
+
+    infoAPublier[0] = "type_msg=BUT";
+    infoAPublier[1] = "type_ent=VB";
+    infoAPublier[2] = "num=1";
+    infoAPublier[3] = data; 
+
+    sendToBeBotte(channel, channelKey, ressource, infoAPublier);	
+}
+
+//Configuration du validateur de but
+void init(char *robots){
+    char *channel = "RJpartieTEST";
+    char *ressource = "msg";
+    //Fonction beta de reception des rfids
+    recvToBebotte(channel, ressource, robots);
+}
+
+char* getIPbyRFID(char *robots, char *rfid){
+	printf("Recherche de l'ip associé à %s..\n", rfid);
+	char *line, *data, *info, *rfid_cmp, *ip, joueurs[4096];
+	char* tabdata[50];
+	int cpt = 0;
+
+	strcpy(joueurs, robots);
+	data = strtok (joueurs, "[]");
+	line = strtok (data, "{}");
+	line = strtok(NULL, "{}");
+	while(line != NULL){
+	    tabdata[cpt] = line;
+	    line = strtok(NULL, "{}");
+	    line = strtok(NULL, "{}");
+	    cpt++;
+	}
+
+	for(int i = 0; i < cpt; i++){
+	    info = strtok(tabdata[i], "\" :");
+	    while(strcmp(info, "data")!=0){
+	    	info = strtok(NULL, "\" :");
+	    }
+	    info = strtok(NULL, "\" :");
+	    rfid_cmp = strtok(info, "=,;");
+	    while(strcmp(rfid_cmp, "RFID")){
+		rfid_cmp = strtok(NULL, "=,;");
+	    }
+	    rfid_cmp = strtok(NULL, "=,;");
+	    if(strcmp(rfid, rfid_cmp) == 0){
+		ip = strtok(NULL, "=,;");
+		ip = strtok(NULL, "=,;");
+		printf("IP associé :%s\n", ip);
+		break;
+	    }
+	}
+
+	return ip;
 }
 
 int main(void){
 
-    char *portname = "/dev/ttyACM1";
+    char *portname = "/dev/ttyACM0";
+    char robots[4096];
     int fd;
     int wlen;
 
+    //Ouverture du port pour écoute du lecteur de carte RFID
     fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
 
     if (fd < 0) {
@@ -130,32 +329,48 @@ int main(void){
     /*baudrate 115200, 8 bits, no parity, 1 stop bit */
     set_interface_attribs(fd, B115200);
 
+
+    /*Configuration du validateur de but*/
+    printf("Initialisation....\n");
+    init(robots);
+    printf("Données récupérées\n\n");
+
     /* attente active */
     do {
         unsigned char rfid[16];
+	unsigned char* ip, idball;
         int rdlen;
 
-
+	printf("En attente d'une RFID...\n");
         rdlen = read(fd, rfid, sizeof(rfid)-1);
 
    	printf("nb lu : %d\n", rdlen);
    	printf("valeur : %s\n", rfid);
-   	printf("taille : %lu\n", sizeof(rfid));
 
-	printf("\nValeur de la rfid : --%s--\n", rfid);
+	printf("\nValeur de la rfid : %s\n", rfid);
 
-	sendMonitoring(rfid, "4685484984");
+	//Recher de l'IP associé à la RFID
+	ip = getIPbyRFID(robots, rfid);
+
+	if(strlen(ip) <= 0){
+		printf("Erreur de reception de la RFID ou aucune IP associée.\n");
+	}else{
+
+		printf("\nIP associé : %s\n", ip);
+
+		//On récupère l'idBall du joueur
+		idball = joueur_request(ip);
+
+		if(1){ //validationbut_1 ("localhost",idball) == 0){
+			printf("But accepté !\n");
+			sendValidBut(ip);
+		}else{
+			printf("But refusé..\n");
+		}
+	}
+	//sendMonitoring(rfid, "4685484984");
         /* repeat for next rfid */
     } while (1);
-
-/* Besoin de récupérer l'ip en fonction de la rfid
-
-    char hostname[256] = "localhost";
-
-    char* idball = joueur_request(hostname);
-    printf("joueur vaut : %s\n", idball);
-    sendMonitoring(hostname, idball);
-*/
   
 }
 
